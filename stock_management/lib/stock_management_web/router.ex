93a3448fc9880bf78e9ca804fd2a59ca8ad42e4c
netocalegari <netokalegari@gmail.com>
2026-01-19 16:06:15 -0300

    feat: adicionando rota /products para resolver erros do scaffold

--- stock_management/lib/stock_management_web/router.ex
+++ stock_management/lib/stock_management_web/router.ex
@@ -18,6 +18,8 @@ defmodule StockManagementWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    resources "/products", ProductController
   end
 
   # Other scopes may use custom stacks.
