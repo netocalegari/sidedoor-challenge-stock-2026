80e72349f5cd4178199c1bfbdfd8bdc82bbdc663
netocalegari <netokalegari@gmail.com>
2026-01-19 17:14:36 -0300

    feat: adicionando regras de negócio para cadastro de produtos

--- stock_management/lib/stock_management/stock/product.ex
+++ stock_management/lib/stock_management/stock/product.ex
@@ -15,6 +15,9 @@ defmodule StockManagement.Stock.Product do
   def changeset(product, attrs) do
     product
     |> cast(attrs, [:name, :description, :quantity, :price])
-    |> validate_required([:name, :description, :quantity, :price])
+    |> validate_required([:name, :quantity, :price])
+    |> validate_length(:name, min: 3)
+    |> validate_number(:quantity, greater_than_or_equal_to: 0)
+    |> validate_number(:price, greater_than_or_equal_to: 0)
   end
 end
