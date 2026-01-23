1844204350dba975f1af5cd4add0847f003ea19e
netocalegari <netokalegari@gmail.com>
2026-01-23 19:17:30 -0300

    feat: endpoint para criação de novos products

--- stock_management/lib/stock_management_web/controllers/api/product_controller.ex
+++ stock_management/lib/stock_management_web/controllers/api/product_controller.ex
@@ -10,4 +10,19 @@ defmodule StockManagementWeb.Api.ProductController do
     product = StockManagement.Stock.get_product!(id)
     render(conn, :show, product: product)
   end
+
+  def create(conn, product_params) do
+    case StockManagement.Stock.create_product(product_params) do
+      {:ok, product} ->
+        conn
+        |> put_status(:created)
+        |> render(:show, product: product)
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(StockManagementWeb.Api.ChangesetView)
+        |> render("error.json", changeset: changeset)
+    end
+  end
 end
